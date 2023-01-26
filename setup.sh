70f77c62cddccb4fe267fdf7f5357cde4038f2e6
Poornima Ramakrishnan <92739776+poorni9r@users.noreply.github.com>
2023-01-26 17:36:45 -0500

    updated nvm version

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/poorni9r/Travel-Demo-app.git
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 16.0.0
 npm install -g http-server
