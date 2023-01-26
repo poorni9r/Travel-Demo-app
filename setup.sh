cf1eea244963ad14b90ed31eb997c0db815892f1
Poornima Ramakrishnan <92739776+poorni9r@users.noreply.github.com>
2023-01-26 17:40:14 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/poorni9r/Travel-Demo-app.git
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install 16.0.0
+nvm install 16
 npm install -g http-server
 cd Travel-Demo-app
 wget https://github.com/elastic/apm-agent-rum-js/releases/download/%40elastic%2Fapm-rum%405.12.0/elastic-apm-rum.umd.min.js
